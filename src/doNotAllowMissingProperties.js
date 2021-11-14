b7e0d29369c4fab83a7f79c3bb1135325a20fd43
Miles Georgi <azimux@gmail.com>
2021-11-14 01:45:12 +0000

    Support private fields in doNotAllowMissingProperties

--- src/doNotAllowMissingProperties.js
+++ src/doNotAllowMissingProperties.js
@@ -1,9 +1,11 @@
-const MissingPropertyError = class extends Error {}
+const { isFunction, isString } = require('lodash')
+
+const MissingPropertyError = class extends Error { }
 
 const PROXY_TARGET_PROPERTY = '__doNotAllowMissingPropertiesProxyTarget'
 const promiseMethods = ['then', 'catch', 'finally']
 
-const throwIfPropertyMissing = {
+const setter = {
   set (object, property, value) {
     if (property in object) {
       object[property] = value
@@ -11,26 +13,39 @@ const throwIfPropertyMissing = {
     }
 
     throwError(property)
-  },
+  }
+}
 
+const getter = {
   get (object, property) {
     if (property === PROXY_TARGET_PROPERTY) {
-      // makes allowMissingProperties function to work
+      // a way to expose the proxy target to make allowMissingProperties function work
       return object
     }
 
-    if (promiseMethods.includes(property)) {
-      // allows code that is interested in if this is a promise via duck typing to work (maybe async/await?)
-      return object
-    }
-
-    if (property in object) {
+    if (property in object || promiseMethods.includes(property) || !isString(property)) {
       return object[property]
     }
 
     throwError(property)
   }
 }
+
+const getterWithPrivateFieldSupport = {
+  get (object, property) {
+    const value = getter.get(object, property)
+
+    if (isMethod(object, property)) {
+      // we want to bind to object so that if invoked this method has access to the
+      // lexical scope it was defined in which will allow private fields to work properly
+      return value.bind(object)
+    }
+    return value
+  }
+}
+
+const accessors = { ...setter, ...getter }
+const accessorsWithPrivateFieldSupport = { ...setter, ...getterWithPrivateFieldSupport }
 
 const throwError = (propertyName) => {
   throw new MissingPropertyError(`Property '${JSON.stringify(propertyName)}' is missing`)
@@ -44,13 +59,22 @@ const allowMissingProperties = (proxy) => {
   return proxy[PROXY_TARGET_PROPERTY] || proxy
 }
 
-const doNotAllowMissingProperties = (object) => {
+const doNotAllowMissingProperties = (object, supportPrivateFields = true) => {
   if (object === undefined || object === null) {
     throw new Error('cannot doNotAllowMissingProperties of ' + object)
   }
 
-  return new Proxy(object, throwIfPropertyMissing)
+  if (supportPrivateFields && isInstanceOfAClass(object)) {
+    return new Proxy(object, accessorsWithPrivateFieldSupport)
+  } else {
+    return new Proxy(object, accessors)
+  }
 }
+
+const isClass = (o) => o && isFunction(o) && /^class /.test(o.toString())
+const isInstanceOfAClass = (o) => isClass(o.constructor)
+const isRegularFunction = (f) => f && isFunction(f) && !isClass(f) && !('prototype' in f)
+const isMethod = (object, property) => object && property && isRegularFunction(object[property]) && isInstanceOfAClass(object)
 
 module.exports = {
   doNotAllowMissingProperties,
--- src/doNotAllowMissingProperties.test.js
+++ src/doNotAllowMissingProperties.test.js
@@ -7,7 +7,13 @@ describe('doNotAllowMissingProperties()', () => {
 
     beforeEach(() => {
       Klass = class {
+        #privateField = 'private field'
+
         exists = 'exists'
+
+        getPrivateField () {
+          return this.#privateField
+        }
       }
 
       instance = doNotAllowMissingProperties(new Klass())
@@ -19,45 +25,51 @@ describe('doNotAllowMissingProperties()', () => {
       })
     })
 
-    describe('read existing property', () => {
+    describe('read existing public field', () => {
       it('returns property value', () => {
         expect(instance.exists).toEqual('exists')
       })
     })
 
-    describe('read non-existing property', () => {
+    describe('read non-existing public field', () => {
       it('throws MissingPropertyError', () => {
         expect(() => instance.doesNotExist).toThrowError(MissingPropertyError)
       })
     })
 
-    describe('write existing property', () => {
+    describe('write existing public field', () => {
       it('sets property value', () => {
         instance.exists = 'new value'
         expect(instance.exists).toEqual('new value')
       })
     })
 
-    describe('write non-existing property', () => {
+    describe('write non-existing public field', () => {
       it('throws MissingPropertyError', () => {
         expect(() => { instance.doesNotExist = 'new value' }).toThrowError(MissingPropertyError)
       })
     })
 
+    describe('public method that accesses private field', () => {
+      it('can access private field as usual', () => {
+        expect(instance.getPrivateField()).toEqual('private field')
+      })
+    })
+
     describe('allowMissingProperties()', () => {
-      describe('read existing property', () => {
+      describe('read existing public field', () => {
         it('returns property value', () => {
           expect(allowMissingProperties(instance).exists).toEqual('exists')
         })
       })
 
-      describe('read non-existing property', () => {
+      describe('read non-existing public field', () => {
         it('returns property value', () => {
           expect(allowMissingProperties(instance).doesNotExist).toEqual(undefined)
         })
       })
 
-      describe('write existing property', () => {
+      describe('write existing public field', () => {
         it('sets property value and returns property value', () => {
           const returnValue = allowMissingProperties(instance).exists = 'new value'
           expect(instance.exists).toEqual('new value')
@@ -66,7 +78,7 @@ describe('doNotAllowMissingProperties()', () => {
         })
       })
 
-      describe('write non-existing property', () => {
+      describe('write non-existing public field', () => {
         it('sets property value and returns property value', () => {
           const returnValue = allowMissingProperties(instance).doesNotExist = 'new value'
           expect(instance.doesNotExist).toEqual('new value')
